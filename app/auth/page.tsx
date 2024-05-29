d6b167731a8cff4e47a4b585414e73574888dc86
Ovodo <ovdizzle@gmail.com>
2024-05-29 10:14:18 +0100

    added sign in page

---
+++ app/auth/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center'>
+      <h3 className='text-3xl text-gray-900 font-bold'>SignIn Successfull</h3>
+    </div>
+  );
+};
+
+export default Page;
